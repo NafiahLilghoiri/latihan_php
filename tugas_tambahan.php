bed4e3e1ab43d6caef6f3805c4a6af12264ed086
NafiahLilghoiri <e42240962@student.polije.ac.id>
2025-09-23 15:22:16 +0700

    latihan PHP

---
+++ tugas_tambahan.php
@@ -0,0 +1,10 @@
+<?php
+// Anggap $tugas1 dan $tugas2 adalah angka string yang ingin digabung
+$tugas1 = "90";
+$tugas2 = "80";
+
+// Menggabungkan string agar menghasilkan nilai gabungan "9080"
+$hasil = $tugas1 . $tugas2; // operator '.' menggabungkan string
+
+echo $hasil; // Output: 9080
+?>
---
+++ variabel.php
@@ -0,0 +1,12 @@
+<?php 
+$nim="E42240962"; 
+$nama="Nafiah"; 
+$prodi="Bisnis Digital";  
+echo "Selamat Praktikum"; 
+echo "<br />";  
+echo $nim; 
+echo "<br />"; 
+echo $nama; 
+echo "<br />"; 
+echo $prodi; 
+?> 

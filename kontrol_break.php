e3c8b2c8542db51c13fee4daa1c133cb05823bdf
NafiahLilghoiri <e42240962@student.polije.ac.id>
2025-09-24 14:51:01 +0700

    latihan kondisi PHP

---
+++ kontrol_break.php
@@ -0,0 +1,26 @@
+//perulangan for//
+<?php
+for ($i=0; $i <100; $i++)
+{
+if ($i==13)
+{
+break;
+}
+echo $i;
+echo "<br />";
+}
+?>
+
+//perulangan while//
+<?php
+$i=0;
+while ($i < 100)
+{
+$i++;
+if ($i==13)
+{
+break;
+}
+echo $i;
+echo "<br />";
+}
---
+++ logika_IF_ELSE.php
@@ -0,0 +1,8 @@
+<?php
+$nama="Nafiah";
+if ($nama=="Nafiah")
+echo "Selamat Datang Nafiah cantik...";
+else
+echo "Selamat Datang di Politeknik Negeri Jember";
+?>
+
